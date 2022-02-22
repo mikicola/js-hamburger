4bf8804b35878a980de2ae6fbd5be422aa36b162
mikicola <michela.colangelo@gmail.com>
2022-02-22 16:13:26 +0100

    aggiungo scomparsa menu

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let hamburgerMenu = document.querySelector(".hamburger-menu")
 let hamburgerIcon = document.querySelector(".fas.fa-bars")
+let hamburgerX = document.querySelector(".fas.fa-times")
+
 
 hamburgerIcon.addEventListener("click",function(){
     hamburgerMenu.classList.add("active")
+})
+
+hamburgerX.addEventListener("click",function(){
+    hamburgerMenu.classList.remove("active")
 })
